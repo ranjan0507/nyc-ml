4b34608d96bf1512272ccb60cec5f13231a08a68
ranjan0507 <ranjanvh05@gmail.com>
2026-02-11 17:28:58 +0530

    add basic ingestion pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,6 +1,40 @@
-import numpy as np
 import pandas as pd
 
-df = pd.read_csv('../data/raw/yellow_tripdata_2020-04.csv')
+RAW_PATH = 'data/raw/raw.csv'
+OUTPUT_PATH = 'data/processed/clean.csv'
 
-print(df.shape)
+REQUIRED_FEATURES = [
+	"fare_amount",
+	"trip_distance",
+    "PULocationID" ,
+	"DOLocationID" ,
+    "passenger_count",
+	"tpep_pickup_datetime",
+	"VendorID",
+	"RatecodeID"
+]
+
+def load_data(path):
+	return pd.read_csv(path)
+
+def clean_data(df):
+	df = df[REQUIRED_FEATURES]
+	df = df.dropna(subset=['fare_amount'])
+	df = df[df['fare_amount']>0]
+	df = df[df['passenger_count']>=1]
+	df = df[df['passenger_count']<=6]
+	return df
+
+def save_processed(df):
+	df.to_csv(OUTPUT_PATH)
+
+def main():
+	df = load_data(RAW_PATH)
+	print('Data Loaded with shape : ', df.shape)
+	df = clean_data(df)
+	print('Clean Data Shape : ',df.shape)
+	save_processed(df)
+	print('Data saved to : ', OUTPUT_PATH)
+
+if __name__=="__main__":
+	main()
